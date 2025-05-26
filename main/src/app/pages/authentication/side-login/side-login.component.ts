a03a0bb3b2ad24165825cbe43e0f213bfd5f47e4
fabio morais <rarinaldo@outlook.com>
2025-05-25 21:46:50 -0300

    sistema de login implementado

--- main/src/app/pages/authentication/side-login/side-login.component.ts
+++ main/src/app/pages/authentication/side-login/side-login.component.ts
@@ -5,14 +5,15 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'src/app/material.module';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-side-login',
-  imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './side-login.component.html',
 })
 export class AppSideLoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   form = new FormGroup({
     uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
@@ -23,7 +24,23 @@ export class AppSideLoginComponent {
     return this.form.controls;
   }
 
-  submit() {
-    this.router.navigate(['']);
+   submit() {
+    if (this.form.invalid) return;
+
+    const payload = {
+      login: this.form.value.uname,
+      password: this.form.value.password,
+    };
+
+    this.http.post('http://localhost:8081/auth/login', payload, { responseType: 'text' })
+      .subscribe({
+        next: (jwt: string) => {
+          localStorage.setItem('jwt', jwt);
+          this.router.navigate(['/dashboard']);
+        },
+        error: () => {
+          alert('Login inválido');
+        }
+      });
   }
 }
